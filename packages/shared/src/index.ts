bc193d60ab02be707a6686c7ebde4ac8449ab639
jiaze <1244153559@qq.com>
2023-05-15 02:14:12 +0800

    feature(shared): 添加公共方法hasChanged

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -5,3 +5,8 @@ export const isArray = Array.isArray
 export const isObject = (val:unknown) => {
   return val !== null && typeof val === 'object'
 }
+
+// 值是否发送改变
+export const hasChanged = (newVal: any, oldVal: any):boolean => {
+  return !Object.is(newVal, oldVal);
+}
