644bc7de5bac467c26cf432dc10afbd3d7dbe865
jiaze <1244153559@qq.com>
2023-05-15 02:15:20 +0800

    feature(ref): ref实现简单数据类型的响应式

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,11 @@
-import { Dep, activeEffect, createDep, trackEffects } from './effect'
+import { hasChanged } from '@myvue/shared'
+import {
+  Dep,
+  activeEffect,
+  createDep,
+  trackEffects,
+  triggerEffects,
+} from './effect'
 import { toReactive } from './reactive'
 
 export interface Ref<T = any> {
@@ -15,6 +22,7 @@ export function myRef(value?: unknown) {
   return createRef(value, false)
 }
 
+// shallow 表示浅层的响应性(即：只有 .value 是响应性的)
 function createRef(rawValue: unknown, shallow: boolean) {
   if (isRef(rawValue)) {
     return rawValue
@@ -25,11 +33,15 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
   private _value: T
+  // 原始数据
+  private _rawValue: T
   public dep?: Dep = undefined
   public readonly __v_isRef: boolean = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
-    // 如果是复杂数据类型 使用reactive
+    this._rawValue = value
+    // 如果 __v_isShallow 为 true，则 value 不会被转化为 reactive 数据，
+    // 即如果当前 value 为复杂数据类型，则会失去响应性。
     this._value = __v_isShallow ? value : toReactive(value)
   }
 
@@ -38,12 +50,26 @@ class RefImpl<T> {
     return this._value
   }
 
-  set value(value: T) {}
+  set value(newVal: T) {
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal
+      // 更新 .value 的值
+      this._value = toReactive(newVal)
+      triggerRefValue(this)
+    }
+  }
 }
 
 // 收集ref依赖
 export function trackRefValue(ref) {
   if (activeEffect) {
     trackEffects(ref.dep || (ref.dep = createDep()))
+  }
+}
+
+// 触发ref依赖
+export function triggerRefValue(ref) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
   }
 }
