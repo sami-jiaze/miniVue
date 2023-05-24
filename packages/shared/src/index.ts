6e12dbb77d9419cc939b0d24eabdde35baa5c5d9
jiaze <1244153559@qq.com>
2023-05-24 14:14:53 +0800

    feature(shared): 添加isFunction工具函数

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,11 +2,16 @@
 export const isArray = Array.isArray
 
 // 是否为对象
-export const isObject = (val:unknown) => {
+export const isObject = (val: unknown) => {
   return val !== null && typeof val === 'object'
 }
 
+// 是否为函数
+export const isFunction = (val: unknown) => {
+  return typeof val === 'function'
+}
+
 // 值是否发送改变
-export const hasChanged = (newVal: any, oldVal: any):boolean => {
-  return !Object.is(newVal, oldVal);
+export const hasChanged = (newVal: any, oldVal: any): boolean => {
+  return !Object.is(newVal, oldVal)
 }
