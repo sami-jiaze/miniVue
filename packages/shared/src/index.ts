49f9e8acb6265d8e3dc8c98052d6a96449bc9fc0
jiaze <1244153559@qq.com>
2023-06-05 17:22:08 +0800

    feature(shared): 添加新公共方法

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -16,4 +16,8 @@ export const hasChanged = (newVal: any, oldVal: any): boolean => {
   return !Object.is(newVal, oldVal)
 }
 
+// 合并
 export const extend = Object.assign
+
+// 空对象
+export const EMPTY_OBJ: { readonly [key: string]: any } = {}
