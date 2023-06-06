f736633fa23841548d002cdf323094202ab87a2b
jiaze <1244153559@qq.com>
2023-06-06 14:13:24 +0800

    feature(runtime-core): 侦听器完善监听功能

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -1,4 +1,4 @@
-import { EMPTY_OBJ, hasChanged } from '@myvue/shared'
+import { EMPTY_OBJ, hasChanged, isObject } from '@myvue/shared'
 import { isReactive } from 'packages/reactivity/src/reactive'
 import { queuePreFlushCb } from './scheduler'
 import { ReactiveEffect } from 'packages/reactivity/src/effect'
@@ -29,7 +29,7 @@ function doWatch(
   if (cb && deep) {
     // TODO
     const baseGetter = getter
-    getter = () => baseGetter()
+    getter = () => traverse(baseGetter())
   }
 
   let oldValue = {}
@@ -59,4 +59,18 @@ function doWatch(
   return () => {
     effect.stop()
   }
+}
+
+export function traverse(value: unknown, seen?: Set<unknown>) {
+  if (!isObject(value)) {
+    return value
+  }
+  seen = seen || new Set()
+
+  seen.add(value)
+
+  for (const key in value as object) {
+    traverse((value as any)[key], seen)
+  }
+  return value
 }
