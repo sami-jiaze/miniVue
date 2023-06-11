cf36079084fc81944dfac7611cf8edb64e79f1de
jiaze <1244153559@qq.com>
2023-06-11 21:33:47 +0800

    feature(runtime): 渲染函数中区别并支持dom属性和attr

--- packages/runtime-dom/src/pathProp.ts
+++ packages/runtime-dom/src/pathProp.ts
@@ -1,14 +1,30 @@
-import { isOn } from "@myvue/shared"
-import { patchClass } from "./modules/class"
+import { isOn } from '@myvue/shared'
+import { patchClass } from './modules/class'
+import { patchDOMProp } from './modules/props'
+import { patchAttr } from './modules/attrs'
 
 export const patchProp = (el: Element, key, prevValue, nextValue) => {
   if (key === 'class') {
+    // console.log(el);
     patchClass(el, nextValue)
-  }else if (key === 'style') {
-
-  } else if(isOn(key)){
-
+  } else if (key === 'style') {
+  } else if (isOn(key)) {
+  } else if (shouldSetAsProp(el, key)) {
+    patchDOMProp(el, key, nextValue)
   } else {
-
+    patchAttr(el, key, nextValue)
   }
+}
+
+function shouldSetAsProp(el: Element, key: string) {
+  if (key === 'form') {
+    return false
+  }
+  if (key === 'list' && el.nodeName === 'INPUT') {
+    return false
+  }
+  if (key === 'type' && el.nodeName === 'TEXTAREA') {
+    return false
+  }
+  return key in el
 }
