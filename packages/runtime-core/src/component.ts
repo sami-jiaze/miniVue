cfa253000afb306560f123d8c27bee853f6aec45
jiaze <1244153559@qq.com>
2023-06-21 17:57:55 +0800

    feature(runtime): 处理渲染有状态的组件

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,3 +1,6 @@
+import { myReactive } from '@myvue/reactivity'
+import { isObject } from '@myvue/shared'
+
 let uid = 0
 
 export function createComponentInstance(vnode) {
@@ -9,7 +12,7 @@ export function createComponentInstance(vnode) {
     subTree: null,
     effect: null,
     update: null,
-    render: null
+    render: null,
   }
   return instance
 }
@@ -23,6 +26,20 @@ function setupStatefulComponent(instance) {
 }
 
 export function finishComponentSetup(instance) {
-  const Component =  instance.type
+  const Component = instance.type
   instance.render = Component.render
+
+  applyOptions(instance)
+}
+
+function applyOptions(instance) {
+  // dataOptions 用来存储 instance.type 对象的 data 属性的值
+  const { data: dataOptions } = instance.type
+
+  if (dataOptions) {
+    const data = dataOptions()
+    if (isObject(data)) {
+      instance.data = myReactive(data)
+    }
+  }
 }
--- packages/runtime-core/src/componentRenderUtils.ts
+++ packages/runtime-core/src/componentRenderUtils.ts
@@ -1,8 +1,8 @@
-import { ShapeFlags } from "packages/shared/src/shapeFlags"
-import { createVNode } from "./vnode"
+import { ShapeFlags } from 'packages/shared/src/shapeFlags'
+import { createVNode } from './vnode'
 
 export function normalizeVNode(child) {
-  if(typeof child === 'object') {
+  if (typeof child === 'object') {
     return child
   } else {
     return createVNode(Text, null, String(child))
@@ -10,14 +10,14 @@ export function normalizeVNode(child) {
 }
 
 export function renderComponentRoot(instance) {
-  const { vnode, render } = instance
+  const { vnode, render, data } = instance
   let result
   try {
-    if(vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT){
-      result = normalizeVNode(render!())
+    if (vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+      result = normalizeVNode(render!.call(data))
     }
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
   return result
 }
