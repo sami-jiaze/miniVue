83c7e42cc04510dbb67260836d5c482316789427
jiaze <1244153559@qq.com>
2023-06-26 01:16:12 +0800

    feature(compile): 除了转化Element节点的函数

--- packages/compiler-core/src/transforms/transformElement.ts
+++ packages/compiler-core/src/transforms/transformElement.ts
@@ -1,3 +1,22 @@
+import { NodeTypes, createVNodeCall } from '../ast'
+
 export const transformElement = (node, context) => {
-  return function postTransformElement() {}
+  return function postTransformElement() {
+    node = context.currentNode
+    if (node.type !== NodeTypes.ELEMENT) {
+      return
+    }
+    const { tag } = node
+    let vnodeTag = `"${tag}"`
+    let vnodeProps = []
+    let vnodeChildren = node.children
+
+    // 核心 新增一个属性
+    node.codegenNode = createVNodeCall(
+      context,
+      vnodeTag,
+      vnodeProps,
+      vnodeChildren,
+    )
+  }
 }
