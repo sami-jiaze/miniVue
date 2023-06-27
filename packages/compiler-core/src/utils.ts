c3d63176d2c23ab2485ffa5d89e6b4b24986e8de
jiaze <1244153559@qq.com>
2023-06-27 20:48:59 +0800

    feature(compiler): VNode生成函数

--- packages/compiler-core/src/utils.ts
+++ packages/compiler-core/src/utils.ts
@@ -1,7 +1,13 @@
 import { NodeTypes } from './ast'
+import { CREATE_ELEMENT_VONDE, CREATE_VNODE } from './runtimeHelprs'
 
 // 是否是text节点
 export function isText(node) {
   return node.type === NodeTypes.INTERPOLATION || node.type === NodeTypes.TEXT
    
+}
+
+// 返回 vnode 生成函数
+export function getVNodeHelper(ssr: boolean, isComponent: boolean) {
+	return ssr || isComponent ? CREATE_VNODE : CREATE_ELEMENT_VONDE
 }
