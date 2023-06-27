bf5c46a7cb5e97b02d5a70e5c4ff9aee3ea797f7
jiaze <1244153559@qq.com>
2023-06-27 21:04:47 +0800

    feature(compile): 解析ast拼接rend

--- packages/compiler-core/src/codegen.ts
+++ packages/compiler-core/src/codegen.ts
@@ -1,12 +1,46 @@
+import { isArray, isString } from '@myvue/shared'
+import { NodeTypes } from './ast'
 import { helperNameMap } from './runtimeHelprs'
+import { getVNodeHelper } from './utils'
 
 export function generate(ast) {
   const context = createCodegenContext(ast)
   const { push, newline, indent, deindent } = context
+  genFunctionPreamble(context)
+
+  const functionName = `render`
+  const args = ['_ctx', '_cache']
+  const signature = args.join(', ')
+  push(`function ${functionName}(${signature}) {`)
+  indent()
+
+  const hasHelpers = ast.helpers.length > 0
+  if (hasHelpers) {
+    push(`const {${ast.helpers.map(aliasHelpr).join(', ')}} = _Vue`)
+    push('\n')
+    newline()
+  }
+
+  newline()
+  push(`return `)
+  if (ast.codegenNode) {
+    genNode(ast.codegenNode, context)
+  } else {
+    push(`null`)
+  }
+
+  deindent()
+  push('}')
+
+  return {
+    ast,
+    code: context.code,
+  }
 }
 
 function createCodegenContext(ast) {
   const context = {
+    isSSR: false,
     code: '',
     runtimeGlobalName: 'Vue',
     source: ast.loc.source,
@@ -24,7 +58,6 @@ function createCodegenContext(ast) {
     },
     indent() {
       newline(++context.indentLevel)
-
     },
     deindent() {
       newline(--context.indentLevel)
@@ -35,3 +68,89 @@ function createCodegenContext(ast) {
   }
   return context
 }
+
+// 生成 "const _Vue = Vue\n\nreturn "
+function genFunctionPreamble(context) {
+  const { newline, push, runtimeGlobalName } = context
+  const VueBinding = runtimeGlobalName
+  push(`const _Vue = ${VueBinding}\n`)
+  newline()
+  push(`return `)
+}
+
+const aliasHelpr = (s: symbol) => `${helperNameMap[s]}: _${helperNameMap[s]}}`
+
+// 节点处理
+function genNode(node, context) {
+  switch (node.type) {
+    case NodeTypes.VNODE_CALL:
+      break
+    case NodeTypes.TEXT:
+      genText(node, context)
+      break
+    case NodeTypes.VNODE_CALL:
+      genVNodeCall(node, context)
+      break
+    default:
+      break
+  }
+}
+
+function genText(node, context) {
+  context.push(JSON.stringify(node.context), node)
+}
+function genVNodeCall(node, context) {
+  const { push, helper } = context
+  const { tag, props, children, patchFlag, dynamicProps, isComponent } = node
+  const callHelper = getVNodeHelper(context.isSSR, isComponent)
+  push(helper(callHelper) + `(`)
+  const args = genNullableArgs([tag, props, children, patchFlag, dynamicProps])
+
+  genNodeList(args, context)
+  push(')')
+}
+
+// 剔除无效参数
+function genNullableArgs(args: any[]) {
+  let i = args.length
+  while (i--) {
+    if (args[i] != null) break
+  }
+  return args.slice(0, i + 1).map((arg) => arg || `null`)
+}
+
+// 参数的填充
+function genNodeList(nodes, context) {
+  const { push, newline } = context
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i]
+    // 字符串直接 push
+    if (isString(node)) {
+      push(node)
+    } else if (isArray(node)) {
+      // 数组需要 push "[" "]"
+      genNodeListAsArray(node, context)
+    } else {
+      // 对象需要区分 node 节点类型，递归处理
+      genNode(node, context)
+    }
+    if (i < nodes.length - 1) {
+      push(', ')
+    }
+  }
+}
+
+function genNodeListAsArray(nodes, context) {
+  context.push(`[`)
+  genNodeList(nodes, context)
+  context.push(`]`)
+}
+
+/**
+`
+const _Vue = Vue
+return function render(_ctx,_cache) {
+  const { createElementVNode: _createElementVNode } = _Vue
+  return _createElementVNode("div",[],["hello world"])
+`
+ */
